ceb5f3be82b2c09db0d11605c111fbcb0e7ae3ba
romncn <romncn@gmail.com>
2020-06-12 12:11:35 +0700

    [+] Start Redesign MrJourneyWeb

--- mrjourney-Fe/src/components/Home/ShowRoomBox.js
+++ mrjourney-Fe/src/components/Home/ShowRoomBox.js
@@ -116,8 +116,8 @@ class ShowRoomBox extends React.Component {
                             {this.state.room.map((room, key) => {
                                 return (
                                     // <span id={key}>
-                                    <div className="col-4">
-                                        <div className="col">
+                                    <div className="col-lg-4 col-md-6 col-sm-12">
+                                        <div className="">
 
                                             <div class="alert box-room show-box">
                                                 <img class="d-block w-100" src={BgSlide1} alt="First slide" />
@@ -189,7 +189,7 @@ class ShowRoomBox extends React.Component {
                                                     </div>
                                                     <div className="navbar pt-2">
                                                         <div>
-                                                            <button type="button" class="btn nav-color round text-white" onClick={this.AlertJoinRoom}>เข้าร่วม</button>
+                                                            <button type="button" class="btn btn-join-color round text-white" onClick={this.AlertJoinWrongCondition}>เข้าร่วม</button>
                                                         </div>
                                                         <div className="button-search">
                                                             <button type="button" className="btn"
@@ -212,8 +212,8 @@ class ShowRoomBox extends React.Component {
 
 
                             {/* Mockup 01 */}
-                            <div className="col-4">
-                                <div className="col">
+                            <div className="col-lg-4 col-md-6 col-sm-12">
+                                <div className="">
 
                                     <div class="alert box-room show-box">
                                         <img class="d-block w-100" src={BgSlide1} alt="First slide" />
@@ -269,7 +269,7 @@ class ShowRoomBox extends React.Component {
                                             </div>
                                             <div className="navbar pt-2">
                                                 <div>
-                                                    <button type="button" class="btn nav-color round text-white" onClick={this.AlertJoinWrongCondition}>เข้าร่วม</button>
+                                                    <button type="button" class="btn nav-color round text-white" onClick={this.AlertJoinRoomSuc}>เข้าร่วม</button>
                                                 </div>
                                                 <div className="button-search">
                                                     <button type="button" className="btn"
@@ -286,84 +286,6 @@ class ShowRoomBox extends React.Component {
                                     </div>
                                 </div>
                             </div>
-
-                            {/* Mockup 02 */}
-                            <div className="col-4">
-                                <div className="col">
-
-                                    <div class="alert box-room show-box">
-                                        <img class="d-block w-100" src={BgSlide1} alt="First slide" />
-                                        <div class="box-room-details show-box mt-2">
-                                            <div className="mt-3 mr-3 ml-3 mb-0">
-                                                <h3 className="py-1">
-                                                    Mockup 02 &nbsp;
-                                                             <button
-                                                        type="button" class="maxMember-btn btn p-0 ml-1 "
-                                                        style={{ fontSize: "12px" }} >
-                                                        0/3
-                                                    </button>
-                                                </h3>
-                                                <span className="py-1" style={{ fontSize: "14px" }}>
-                                                    Room ID : R_000003
-                                                </span>
-                                                <br />
-                                                <span className="py-1" style={{ fontSize: "14px" }}>
-                                                    จังหวัด : กรุงเทพมหานคร
-                                                </span>
-                                                <br />
-                                                <span className="py-1" style={{ fontSize: "14px" }}>
-                                                    วันที่ : &nbsp;
-                                                            <button
-                                                        type="button" class="show-details-btn btn p-1 " style={{ fontSize: "10px" }}>
-                                                        {momentjs('2020-06-08').format('DD/MM/YYYY')}
-                                                        <i class="far fa-calendar-alt ml-2 mr-1"></i>
-                                                    </button>
-                                                            &nbsp; - &nbsp;
-                                                            <button
-                                                        type="button" class="show-details-btn btn p-1" style={{ fontSize: "10px" }}>
-                                                        {momentjs('2020-06-10').format('DD/MM/YYYY')}
-                                                        <i class="far fa-calendar-alt ml-2 mr-1"></i>
-                                                    </button>
-                                                </span>
-                                                <p />
-                                                <div className="text-right">
-                                                    <span className="Show-genderCondition pl-2 pr-2">
-                                                        <i class="fas fa-user fa-lg ml-2 mb-0" style={{ color: "hotpink" }}></i>
-                                                        <i class="fas fa-user fa-lg ml-2" style={{ color: "dodgerblue" }}></i>
-                                                    </span>
-                                                    <br /><span className="mt-0 ml-2" style={{ fontSize: "10px" }}>
-                                                        อายุ
-                                                        &nbsp;
-                                                            <button
-                                                            type="button" class="show-details-btn btn p-1 " style={{ fontSize: "8px" }}>
-                                                            ไม่จำกัดช่วงอายุ
-                                                        </button>
-                                                    </span>
-                                                </div>
-                                                <span className="pl-1 pr-1"><img src={Logo} class="image_outer_container" height="30px" width="30px" alt="owner-img" /></span>
-                                                <span style={{ fontSize: "13px" }}>ผู้สร้าง : mrjourney</span>
-                                            </div>
-                                            <div className="navbar pt-2">
-                                                <div>
-                                                    <button type="button" class="btn nav-color round text-white" onClick={this.AlertJoinRoomDontAcc}>เข้าร่วม</button>
-                                                </div>
-                                                <div className="button-search">
-                                                    <button type="button" className="btn"
-                                                        onClick={() => this.setState({ addModalShow: true })}>
-                                                        <i class="fas fa-search fa-2x" style={{ color: "#F37945" }}></i>
-                                                    </button>
-                                                    <MoreRoomDetailModal
-                                                        show={this.state.addModalShow}
-                                                        onHide={this.addModalClose} //use for closeButton
-                                                    ></MoreRoomDetailModal>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-
                         </div>
                     </div>
                 </div>
--- mrjourney-Fe/src/components/Nav/NavWebPage.js
+++ mrjourney-Fe/src/components/Nav/NavWebPage.js
@@ -45,7 +45,7 @@ class NavWebPage extends React.Component {
             text: 'Login ก่อนสิ!',
             showCancelButton: false,
             confirmButtonColor: '#F37945',
-            confirmButtonText:  '<a href ="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1653975470&redirect_uri=http://localhost:3000/Home&scope=profile%20openid%20email&state=KZKEMsjQOZM3uvnZ" id="alert-confirm-button">Login</a>'
+            confirmButtonText: '<a href ="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1653975470&redirect_uri=http://localhost:3000/Home&scope=profile%20openid%20email&state=KZKEMsjQOZM3uvnZ" id="alert-confirm-button">Login</a>'
         })
     }
 
@@ -85,23 +85,23 @@ class NavWebPage extends React.Component {
 
     render() {
         return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-dark nav-color" style={{ color: "white" }}>
+            <div className="navbar-webpage">
+                <nav className="navbar navbar-expand-lg navbar-dark" style={{ color: "white" }}>
                     <a href="/Home" className="navbar-brand">
                         <img src={Logo} height="45" alt="MrJourney" />
                     </a>
-                    <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                        <span className="navbar-toggler-icon"></span>
+                    <button type="button" className="navbar-toggler " data-toggle="collapse" data-target="#navbarCollapse" style={{color:"#2b2b2b",borderColor:"lightgrey"}}>
+                        <i class="fas fa-hamburger" ></i>
                     </button>
 
                     {
                         this.state.displayName === "" ? // state ? true : false
                             <div className="collapse navbar-collapse" id="navbarCollapse">
                                 <ul className="navbar-nav ml-auto">
-                                    <li className="nav-item mr-1 mt-1">
+                                    <li className="nav-item mr-1 mt-1 pt-1">
                                         <button className="btn btn-outline-light my-2 my-sm-0" onClick={() => this.OpenSearch()}>Search</button>
                                     </li>
-                                    <li className="nav-item mr-1 mt-1">
+                                    <li className="nav-item mr-1 mt-1 pt-1">
                                         {
                                             this.state.showSearch ? // state ? true : false
                                                 <form className="form-inline">
@@ -111,18 +111,18 @@ class NavWebPage extends React.Component {
                                                 : null
                                         }
                                     </li>
-                                    <li className="nav-item mr-1 mt-1">
+                                    <li className="nav-item mr-1 mt-1 pt-1">
                                         <button type="button"
-                                            className="btn btn-light ml-2 mr-2 text-dark round"
+                                            className="btn create-btn ml-2 mr-2 text-dark round"
                                             onClick={this.AlertTrip}>
                                             Create Trip
                                             <i className="fas fa-plus fa-sm ml-1" style={{ color: "dark" }}></i>
                                         </button>
                                     </li>
 
-                                    <li className="nav-item mt-1">
+                                    <li className="nav-item mt-1 pt-1">
                                         <button type="button"
-                                            className="btn btn-light ml-2 mr-2 text-dark round"
+                                            className="btn create-btn ml-2 mr-2 text-dark round"
                                             onClick={this.AlertRoom}>
                                             Create Room
                                             <i className="fas fa-plus fa-sm ml-1" style={{ color: "dark" }}></i>
@@ -132,8 +132,7 @@ class NavWebPage extends React.Component {
                                         <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
                                             aria-haspopup="true" aria-expanded="false">
 
-                                            <i> <img src={IconProfile} class="login-profile" height="32px" width="32px" alt="owner-img" /> </i>
-                                            <span className="text-white mt-1 pr-1">Guest</span>
+                                            <i> <img src={IconProfile} class="login-profile" height="37px" width="37px" alt="owner-img" /> </i>
                                         </a>
 
                                         <div className="dropdown-menu dropdown-menu-right dropdown-info"
@@ -147,59 +146,74 @@ class NavWebPage extends React.Component {
                             :
                             <div className="collapse navbar-collapse" id="navbarCollapse">
                                 <ul className="navbar-nav ml-auto">
-                                    <li className="nav-item mr-1 mt-1">
-                                        <button className="btn btn-outline-light my-2 my-sm-0" onClick={() => this.OpenSearch()}>Search</button>
+                                    <li className="nav-item mr-1 mt-1 pt-1">
+                                        <button className="btn search-btn my-2 my-sm-0" onClick={() => this.OpenSearch()} style={{ height: "40px" }}>Search</button>
                                     </li>
-                                    <li className="nav-item mr-1 mt-1">
+                                    <li className="nav-item mr-1 mt-1 pt-1" >
                                         {
                                             this.state.showSearch ? // state ? true : false
                                                 <form className="form-inline">
                                                     <input className="form-control mr-sm-2"
-                                                        type="search" placeholder="Search" aria-label="Search" />
+                                                        type="search" placeholder="Search" aria-label="Search" style={{ height: "40px" }} />
                                                 </form>
                                                 : null
                                         }
                                     </li>
-                                    <li className="nav-item mr-1 mt-1">
+                                    <li className="nav-item mr-1 mt-1 pt-1">
                                         <Link to="/CreateTrip">
-                                            <button type="button" className="btn btn-light ml-2 mr-2 text-dark round">Create Trip
+                                            <button type="button" className="btn create-btn ml-2 mr-2 text-white" style={{ height: "40px" }}>Create Trip
                                                  <i className="fas fa-plus fa-sm ml-1" style={{ color: "dark" }}></i>
                                             </button>
                                         </Link>
                                     </li>
 
-                                    <li className="nav-item mt-1">
+                                    <li className="nav-item mt-1 pt-1">
                                         <Link to="/CreateJoinRoom">
-                                            <button type="button" className="btn btn-light ml-2 mr-2 text-dark round">Create Room
+                                            <button type="button" className="btn create-btn ml-2 mr-2 text-white" style={{ height: "40px" }}>Create Room
                                                  <i className="fas fa-plus fa-sm ml-1" style={{ color: "dark" }}></i>
                                             </button>
                                         </Link>
                                     </li>
                                     <li className="nav-item dropdown ">
-                                        <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
+                                        <a className="nav-link dropdown-toggle profile-session" data-toggle="dropdown"
                                             aria-haspopup="true" aria-expanded="false">
-
-                                            <i> <img src={this.state.pictureURL} class="login-profile" height="32px" width="32px" alt="owner-img" /> </i>
-                                            <span className="text-white mt-1 pr-1">{this.state.displayName}</span>
+                                            <i> <img src={this.state.pictureURL} class="login-profile" height="40px" width="40px" alt="owner-img" /> </i>
+                                            <i class="fas fa-chevron-down fa-sm mt-1" style={{ color: "#585858" }}></i>
                                         </a>
-
                                         <div className="dropdown-menu dropdown-menu-right dropdown-info"
-                                            aria-labelledby="navbarDropdownMenuLink-4">
-                                            <a href="/Profile"
-                                                className="dropdown-item">My Profile</a>
-                                            <a href="/"
-                                                className="dropdown-item">Joied Room</a>
-                                            <a href="/"
-                                                className="dropdown-item">Owner Room</a>
-                                            <button
-                                                className="dropdown-item" onClick={this.onLogout}>Sign Out</button>
+                                        >
+                                            <div className="ml-1 mr-1 dropdown-show-profile px-2">
+                                                <img src={this.state.pictureURL} class="login-profile" height="45px" width="45px" alt="owner-img" />
+                                                <span className="btn ml-3 dropdown-profile-label">{this.state.displayName}</span>
+                                            </div>
+                                            <div className="my-2 dropdown-item">
+                                                <a href="/Profile" className=" a-dropdown"
+                                                ><i class="fas fa-user-circle pr-1 fa-lg"
+                                                    /> My Profile</a>
+                                            </div>
+                                            <div className="my-2 dropdown-item">
+                                                <a href="/Profile" className=" a-dropdown"
+                                                ><i class="fas fa-door-open pr-1 fa-lg"
+                                                    /> Join Room</a>
+                                            </div>
+                                            <div className="my-2 dropdown-item">
+                                                <a href="/Profile" className=" a-dropdown"
+                                                ><i class="fas fa-house-user pr-1 fa-lg"
+                                                    /> Owner Room</a>
+                                            </div>
+                                            <div className="my-2 dropdown-item signout-btn  a-dropdown">
+                                                <a onClick={this.onLogout}
+                                                ><i class="fas fa-sign-out-alt pr-1 fa-lg"
+                                                    /> Sign Out</a>
+                                            </div>
                                         </div>
+
 
                                     </li>
                                 </ul>
                             </div>
                     }
-                        
+
                 </nav>
             </div >
         )
