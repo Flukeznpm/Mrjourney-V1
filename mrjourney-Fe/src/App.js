81b9db37723befce252bba8bdd6f34da1604240e
romncn <romncn@gmail.com>
2020-08-05 23:40:19 +0700

    [+] Chage Route from index.js To App.js

--- mrjourney-Fe/src/App.js
+++ mrjourney-Fe/src/App.js
@@ -1,13 +1,58 @@
 import React from 'react';
 import './static/css/App.css';
-import Home from './webpage/Home'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import CreateTrip from './linepage/CreateTrip';
+import Home from './webpage/Home';
+import CreateJoinRoom from './webpage/CreateJoinRoom';
+import JoinedRoom from './webpage/JoinedRoom';
+import MyOwnerRoom from './webpage/MyOwnerRoom';
+import CurrentJoinRoom from './webpage/CurrentJoinRoom';
+import Profile from './webpage/Profile';
+import CheckTrip from './linepage/CheckTrip';
+import LoginPage from './webpage/LoginPage';
+import FirstTimeLogin from './webpage/FirstTimeLogin';
 
 function App() {
-    return (
-      <div>
-        <Home></Home>
-      </div>
-    );
-  }
+  return (
+    <Router>
+      <Switch>
+        <Route path="/Home">
+          <Home />
+        </Route>
+        <Route path="/CheckTrip" >
+          <CheckTrip />
+        </Route>
+        <Route path="/CreateTrip">
+          <CreateTrip />
+        </Route>
+        <Route path="/CreateJoinRoom" >
+          <CreateJoinRoom />
+        </Route>
+        <Route path="/JoinedRoom">
+          <JoinedRoom />
+        </Route>
+        <Route path="/Profile">
+          <Profile />
+        </Route>
+        <Route path="/MyOwnerRoom">
+          <MyOwnerRoom />
+        </Route>
+        <Route path="/CurrentJoinRoom">
+          <CurrentJoinRoom />
+        </Route>
+        <Route path="/LoginPage">
+          <LoginPage />
+        </Route>
+        <Route path="/FirstTimeLogin">
+          <FirstTimeLogin />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
 
 export default App;
--- mrjourney-Fe/src/index.js
+++ mrjourney-Fe/src/index.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-} from 'react-router-dom';
 import App from './App';
-import CreateTrip from './linepage/CreateTrip';
-import Home from './webpage/Home';
-import CreateJoinRoom from './webpage/CreateJoinRoom';
-import JoinedRoom from './webpage/JoinedRoom';
-import MyOwnerRoom from './webpage/MyOwnerRoom';
-import CurrentJoinRoom from './webpage/CurrentJoinRoom';
-import Profile from './webpage/Profile';
-import CheckTrip from './linepage/CheckTrip';
-import LoginPage from './webpage/LoginPage';
-import FirstTimeLogin from './webpage/FirstTimeLogin';
 import { HookProvider } from './store/HookProvider'
 import momentjs from 'moment'
 import 'moment/locale/th'
@@ -23,43 +8,7 @@ momentjs.locale('th')
 
 ReactDOM.render(
     <HookProvider>
-        <Router>
-            <Switch>
-                <Route path="/CheckTrip" >
-                    <CheckTrip />
-                </Route>
-                <Route path="/CreateTrip">
-                    <CreateTrip />
-                </Route>
-                <Route path="/CreateJoinRoom" >
-                    <CreateJoinRoom />
-                </Route>
-                <Route path="/JoinedRoom">
-                    <JoinedRoom />
-                </Route>
-                <Route path="/Home">
-                    <Home />
-                </Route>
-                <Route path="/Profile">
-                    <Profile />
-                </Route>
-                <Route path="/MyOwnerRoom">
-                    <MyOwnerRoom />
-                </Route>
-                <Route path="/CurrentJoinRoom">
-                    <CurrentJoinRoom />
-                </Route>
-                <Route path="/LoginPage">
-                    <LoginPage />
-                </Route>
-                <Route path="/FirstTimeLogin">
-                    <FirstTimeLogin />
-                </Route>
-                <Route path="/">
-                    <App />
-                </Route>
-            </Switch>
-        </Router>
+        <App></App>
     </HookProvider>
     , document.getElementById('root')
 );
