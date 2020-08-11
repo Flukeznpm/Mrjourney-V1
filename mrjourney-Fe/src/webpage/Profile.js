e6838b6f345f563f5c31522901ccdf4fa6eb8866
romncn <romncn@gmail.com>
2020-08-11 19:43:34 +0700

    [+] Profile Hook

--- mrjourney-Fe/src/webpage/Profile.js
+++ mrjourney-Fe/src/webpage/Profile.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import NavWebPage from '../components/Nav/NavWebPage';
 import ProfileImg from '../static/img/bg-slide-test-1.jpg';
 import '../static/css/App.css';
 import FooterWebPage from '../components/Footer/FooterWebPage';
 import { Tabs, Tab } from 'react-bootstrap'
 import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 import jwt from 'jsonwebtoken';
 import cookie from 'react-cookies'
 
@@ -16,95 +17,73 @@ const ProfileMoreDetail = () => {
                 id="controlled-tab-example"
                 activeKey={key}
                 onSelect={(k) => setKey(k)}>
-                <Tab eventKey="home" title="Home"> sadass</Tab>
-                <Tab eventKey="profile" title="Profile">sfsafs</Tab>
-                <Tab eventKey="contact" title="Contact" disabled>sfsaf</Tab>
+                <Tab eventKey="Bio" title="Bio">Welcome to MyProfile</Tab>
+                <Tab eventKey="HisRoom" title="History Room">ShowRoom</Tab>
+                <Tab eventKey="HisTrip" title="History Trip">ShowTrip</Tab>
             </Tabs>
         </div>
     )
 }
 
-class Profile extends React.Component {
+function Profile(props) {
+    const [lineID, setLineID] = useState("")
+    const [displayName, setDisplayName] = useState("")
+    const [pictureURL, setPictureURL] = useState("")
+    const [acc, setShowAcc] = useState([{}])
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            displayName: '',
-            pictureURL: '',
-            email: ''
-        }
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         let loadJWT = cookie.load('jwt');
         console.log(loadJWT)
         if (loadJWT === undefined) {
-            this.props.history.push('/Home');
+            props.history.push('/Home');
         } else {
             var user = jwt.verify(loadJWT, 'secreatKey');
-            this.setState({
-                displayName: user.displayName,
-                pictureURL: user.pictureURL,
-                email: user.email,
-            })
+            setDisplayName(user.displayName)
+            setPictureURL(user.pictureURL)
+            setLineID(user.lineID)
         }
-    }
+        axios.get(`http://localhost:5000/accountProfile?lineID=${user.lineID}`)
+            .then(async res => {
+                console.log('axios data:' + res)
+                setShowAcc(res.data)
+                console.log('acc', acc)
+            })
+    }, [])
 
-    render() {
-        return (
-            <div className="flex-wrapper">
-                <div className="top-page">
-                    <NavWebPage />
+    return (
+        <div className="flex-wrapper">
+            <div className="top-page">
+                <NavWebPage />
 
-                    <div className="content-page">
-                        <div className="pt-5">
-                            <div className="Profile-Details text-center">
-                                <img src={this.state.pictureURL} class="image_outer_container" height="200px" width="200px" alt="mrjourney-img" />
-                                <div className="line-name pt-2" style={{ fontSize: "28px" }}>คุณ : {this.state.displayName}</div>
-                                <div className="line-name pt-2" style={{ fontSize: "20px" }}>เมล : {this.state.email}</div>
-                                <div className="detail-web pt-2">
-                                    <span>ชื่อจริง : </span>
-                                    <p /><span>เพศ : </span>
-                                </div>
+                <div className="content-page">
+                    <div className="pt-5">
+                        <div className="Profile-Details text-center">
+                            <img src={pictureURL} class="image_outer_container" height="200px" width="200px" alt="mrjourney-img" />
+                            <div className="line-name pt-2" style={{ fontSize: "28px" }}>คุณ : {displayName}</div>
+                            <div className="detail-web pt-2">
+                                <span>ชื่อจริง : {acc.fName} </span>
+                                <p /><span>เพศ : </span>
                             </div>
-                            <div className="container">
-                                <div className="Profile-show-box mt-2" >
-                                    <ProfileMoreDetail></ProfileMoreDetail>
-                                    {/* <div className="bg-warning" style={{ width: "100%", height: "200px" }}> */}
-                                    {/* <div className="navbar">
-                                        <div className="Bio-box">
-                                            Bio
+                        </div>
+                        <div className="container">
+                            <div className="Profile-show-box mt-2" >
+                                <ProfileMoreDetail></ProfileMoreDetail>
+                            </div>
+                            <div className="Profile-score py-2 mt-5">
+                                <div className="container">
+                                    <div className="text-left pl-3">คะแนน</div><p />
+                                    <div className="row text-center">
+                                        <div className="col-4">
+                                            <img src={ProfileImg} class="image_outer_container" height="200px" width="200px" alt="mrjourney-img" />
+                                            <div className="pt-2">ความสนุก</div>
                                         </div>
-                                        <div className="Bio-box">
-                                            History Join Room
+                                        <div className="col-4">
+                                            <img src={ProfileImg} class="image_outer_container" height="200px" width="200px" alt="mrjourney-img" />
+                                            <div className="pt-2">ความคุ้มค่า</div>
                                         </div>
-                                        <div className="Bio-box">
-                                            History Trip
-                                        </div>
-                                    </div>
-                                    <div className="display-show-box">
-
-                                    </div> */}
-
-                                    {/* </div> */}
-
-                                </div>
-                                <div className="Profile-score py-2 mt-5">
-                                    <div className="container">
-                                        <div className="text-left pl-3">คะแนน</div><p />
-                                        <div className="row text-center">
-                                            <div className="col-4">
-                                                <img src={ProfileImg} class="image_outer_container" height="200px" width="200px" alt="mrjourney-img" />
-                                                <div className="pt-2">ความสนุก</div>
-                                            </div>
-                                            <div className="col-4">
-                                                <img src={ProfileImg} class="image_outer_container" height="200px" width="200px" alt="mrjourney-img" />
-                                                <div className="pt-2">ความคุ้มค่า</div>
-                                            </div>
-                                            <div className="col-4">
-                                                <img src={ProfileImg} class="image_outer_container" height="200px" width="200px" alt="mrjourney-img" />
-                                                <div className="pt-2">การจัดการแผน</div>
-                                            </div>
+                                        <div className="col-4">
+                                            <img src={ProfileImg} class="image_outer_container" height="200px" width="200px" alt="mrjourney-img" />
+                                            <div className="pt-2">การจัดการแผน</div>
                                         </div>
                                     </div>
                                 </div>
@@ -112,11 +91,11 @@ class Profile extends React.Component {
                         </div>
                     </div>
                 </div>
-                <div className="footer-page">
-                    <FooterWebPage></FooterWebPage>
-                </div>
             </div>
-        )
-    }
+            <div className="footer-page">
+                <FooterWebPage></FooterWebPage>
+            </div>
+        </div>
+    )
 }
 export default withRouter(Profile);
