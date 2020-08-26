dc9e912f7122df56bb1f9a83fe7de0d38c624996
romncn <romncn@gmail.com>
2020-08-27 01:47:21 +0700

    [+] Can check to show OwnerRoom card or not when u dont have ownerRoom

--- mrjourney-Fe/src/webpage/AllMyOwnerRoom.js
+++ mrjourney-Fe/src/webpage/AllMyOwnerRoom.js
@@ -43,80 +43,84 @@ function MyOwnerRoom(props) {
                     <h1 className="font-weight-bold">ห้องที่คุณเป็นเจ้าของ</h1>
                     <div className="show-owner-room">
                         <div className="row">
-                            {ownerRoom.map((ownerRoom) => {
-                                return (
-                                    <>
-                                        {ownerRoom ?
-                                            <div className="col-md-4 col-sm-12 d-flex justify-content-center py-2">
-                                                <div class="card" style={{ width: "18rem" }}>
-                                                    <img class="card-img-top" src={BgSlide1} alt="Card image cap" />
-                                                    <div class="card-body">
-                                                        <h4 class="card-title">
-                                                            {ownerRoom.roomName} &nbsp;
-                                            <button
-                                                                type="button" class="float-right maxMember-btn btn p-0 "
-                                                                style={{ fontSize: "10px" }} >
-                                                                0/
-                                                        {ownerRoom.maxMember}
-                                                            </button>
-                                                        </h4>
-                                                        <div class="card-text">จ. {ownerRoom.province}</div>
-                                                        <div class="card-text py-2">
-                                                            <button
-                                                                type="button" class="date-room-btn btn p-1 " style={{ fontSize: "12px" }}>
-                                                                {momentjs(ownerRoom.startDate).format('ll')}                                                        <i class="far fa-calendar-alt ml-2 mr-1"></i>
-                                                            </button>
-                                                    &nbsp;-&nbsp;
-                                                <button
-                                                                type="button" class="date-room-btn btn p-1 " style={{ fontSize: "12px" }}>
-                                                                {momentjs(ownerRoom.endDate).format('ll')}                                                        <i class="far fa-calendar-alt ml-2 mr-1"></i>
-                                                            </button>
-                                                        </div>
-                                                        <div className="card-text py-2">
-                                                            {ownerRoom.genderCondition === 'ชาย' ?
-                                                                <span className="Show-genderCondition pl-2 pr-2" style={{ fontSize: "0.75rem" }}>
-                                                                    <i class="fas fa-user fa-lg ml-2" style={{ color: "dodgerblue" }}></i>
+                            {ownerRoom.length ?
+                                <>
+                                    {ownerRoom.map((ownerRoom) => {
+                                        return (
+                                            <>
+                                                <div className="col-md-4 col-sm-12 d-flex justify-content-center py-2">
+                                                    <div class="card" style={{ width: "18rem" }}>
+                                                        <img class="card-img-top" src={BgSlide1} alt="Card image cap" />
+                                                        <div class="card-body">
+                                                            <h4 class="card-title">
+                                                                {ownerRoom.roomName} &nbsp;
+                                                                <button
+                                                                    type="button" class="float-right maxMember-btn btn p-0 "
+                                                                    style={{ fontSize: "10px" }} >
+                                                                    0/
+                                                                    {ownerRoom.maxMember}
+                                                                </button>
+                                                            </h4>
+                                                            <div class="card-text">จ. {ownerRoom.province}</div>
+                                                            <div class="card-text py-2">
+                                                                <button
+                                                                    type="button" class="date-room-btn btn p-1 " style={{ fontSize: "12px" }}>
+                                                                    {momentjs(ownerRoom.startDate).format('ll')}                                                        <i class="far fa-calendar-alt ml-2 mr-1"></i>
+                                                                </button>
+                                                                &nbsp;-&nbsp;
+                                                                <button
+                                                                    type="button" class="date-room-btn btn p-1 " style={{ fontSize: "12px" }}>
+                                                                    {momentjs(ownerRoom.endDate).format('ll')}                                                        <i class="far fa-calendar-alt ml-2 mr-1"></i>
+                                                                </button>
+                                                            </div>
+                                                            <div className="card-text py-2">
+                                                                {ownerRoom.genderCondition === 'ชาย' ?
+                                                                    <span className="Show-genderCondition pl-2 pr-2" style={{ fontSize: "0.75rem" }}>
+                                                                        <i class="fas fa-user fa-lg ml-2" style={{ color: "dodgerblue" }}></i>
+                                                                    </span>
+                                                                    :
+                                                                    ""}
+                                                                {ownerRoom.genderCondition === 'หญิง' ?
+                                                                    <span className="Show-genderCondition pl-2 pr-2" style={{ fontSize: "0.75rem" }}>
+                                                                        <i class="fas fa-user fa-lg ml-2 mb-0" style={{ color: "hotpink" }}></i>
+                                                                    </span>
+                                                                    :
+                                                                    ""}
+                                                                {ownerRoom.genderCondition === 'ไม่จำกัดเพศ' ?
+                                                                    <span className="Show-genderCondition pl-2 pr-2" style={{ fontSize: "0.75rem" }}>
+                                                                        <i class="fas fa-user fa-lg ml-2 mb-0" style={{ color: "hotpink" }}></i>
+                                                                        <i class="fas fa-user fa-lg ml-2" style={{ color: "dodgerblue" }}></i>
+                                                                    </span>
+                                                                    :
+                                                                    ""}
+                                                                <span className="mt-0 ml-2" style={{ fontSize: "12px" }}>
+                                                                    อายุ {ownerRoom.ageCondition}
                                                                 </span>
-                                                                :
-                                                                ""}
-                                                            {ownerRoom.genderCondition === 'หญิง' ?
-                                                                <span className="Show-genderCondition pl-2 pr-2" style={{ fontSize: "0.75rem" }}>
-                                                                    <i class="fas fa-user fa-lg ml-2 mb-0" style={{ color: "hotpink" }}></i>
-                                                                </span>
-                                                                :
-                                                                ""}
-                                                            {ownerRoom.genderCondition === 'ไม่จำกัดเพศ' ?
-                                                                <span className="Show-genderCondition pl-2 pr-2" style={{ fontSize: "0.75rem" }}>
-                                                                    <i class="fas fa-user fa-lg ml-2 mb-0" style={{ color: "hotpink" }}></i>
-                                                                    <i class="fas fa-user fa-lg ml-2" style={{ color: "dodgerblue" }}></i>
-                                                                </span>
-                                                                :
-                                                                ""}
-                                                            <span className="mt-0 ml-2" style={{ fontSize: "12px" }}>
-                                                                อายุ {ownerRoom.ageCondition}
-                                                            </span>
-                                                        </div>
-                                                        <div className="owner-trip-profile py-2">
-                                                            <span className="pl-1 pr-2"><img src={ownerRoom.ownerPicRoom} class="image_outer_container" height="35px" width="35px" alt="owner-img" /></span>
-                                                            <span className="pl-1" style={{ fontSize: "13px" }}>ผู้สร้าง : {ownerRoom.ownerRoomName}</span>
-                                                        </div>
-                                                        <button type="button" class="col-5 mx-2 btn btn-outline-danger round">
-                                                            ลบห้อง
+                                                            </div>
+                                                            <div className="owner-trip-profile py-2">
+                                                                <span className="pl-1 pr-2"><img src={ownerRoom.ownerPicRoom} class="image_outer_container" height="35px" width="35px" alt="owner-img" /></span>
+                                                                <span className="pl-1" style={{ fontSize: "13px" }}>ผู้สร้าง : {ownerRoom.ownerRoomName}</span>
+                                                            </div>
+                                                            <button type="button" class="col-5 mx-2 btn btn-outline-danger round">
+                                                                ลบห้อง
                                                         </button>
-                                                        <Link to="/JoinRoom">
-                                                            <button type="button" className="btn mx-2 col-5 btn-join-color round text-white">
-                                                                ข้อมูลห้อง
+                                                            <Link to="/JoinRoom">
+                                                                <button type="button" className="btn mx-2 col-5 btn-join-color round text-white">
+                                                                    ข้อมูลห้อง
                                                         </button>
-                                                        </Link>
+                                                            </Link>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            :
-                                            null
-                                        }
-                                    </>
-                                )
-                            })}
+                                            </>
+                                        )
+                                    })}
+                                </>
+                                :
+                                <h2 className="col-12 font-weight-bold text-center color-default py-4">
+                                    ขณะนี้ยังไม่มีห้องที่ท่านเป็นเจ้าของ
+                                </h2>
+                            }
                         </div>
                     </div>
                 </div>
