32df4a248158b6766addbcc1413a4358dc9880db
Flukeznpm <nattapong.meun@gmail.com>
2020-09-18 15:39:08 +0700

    [+] api checkUserRegister

--- mrjourney-Be/controller/updateController.js
+++ mrjourney-Be/controller/updateController.js
@@ -5,18 +5,39 @@ let db = firebase.firestore()
 
 //---------------- Controller ----------------//
 // POST /update (อัพเดททุกอย่าง)
+// POST /checkUserRegister (API สำหรับเช็คว่าUserมีอยู่ในระบบหรือเปล่า และใช้ในกรณีที่Userหนีการลงทะเบียน)
 // - อัพเดทข้อมูลชื่อและรูปภาพของ user
 // - อัพเดทเมื่อ user ออกจากกลุ่ม line ไป
 // - save lineID Displayname PictureURL 
 
 router.post('/', async function (req, res, next) {
-    let datas = req.body
+    let datas = req.body;
     // console.log('datas : ' , datas)
     await updateProfile(datas);
     res.status(201).json({
         message: "Update DisplayName and PictureURL success",
     })
 });
+
+router.post('/checkUserRegister', async function (req, res, next) {
+    console.log('Data: ', req.body);
+    let lineID = req.body.lineID;
+    if (lineID == undefined || lineID == null || lineID == '') {
+        console.log('Alert: The Data was empty or undefined"')
+        return;
+    } else {
+        let CheckUserRegister = await db.collection('AccountProfile').doc(dataLine.lineID);
+        CheckUserRegister.get().then(async datas => {
+            if (datas.exists) {
+                console.log('User have account in system')
+                res.status(200);
+            } else {
+                console.log('User do not have account in system')
+                res.status(202);
+            }
+        })
+    }
+})
 
 //---------------- Function ----------------//
 async function updateProfile(data) {
