332ab11c98fec3d98b984e500860fac18d64d202
chouxchu <chutikan.khuanpan@hotmail.com>
2020-09-20 19:45:59 +0700

    [🔧] API weather (change api)

--- mrjourney-Be/lineBot/bot.js
+++ mrjourney-Be/lineBot/bot.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 const request = require('request');
@@ -5,68 +6,115 @@ const request = require('request');
 
 router.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    let msg = req.body.events[0].message.text
-    if (msg === "อาจารย์โอ๋") {
-        replyProfessor(reply_token, msg)
-    }
-    else if (msg === "#สร้างทริป") {
-        replyCreate(reply_token, msg)
-    }
-    else if (msg === "#ดูแผน") {
-        replyPlan(reply_token, msg)
-    }
-    else if (msg === "ดูแผนทั้งหมด") {
-        replyPlanPerDay(reply_token, msg)
-    }
-    else if (msg === "ดูแผนวันนี้") {
-        replyPlanPerDay(reply_token, msg)
-    }
-    else if (msg === "#ช่วย") {
-        replyHelp(reply_token, msg)
-    }
-    else if (msg === "วิธีการสร้างทริป") {
-        replyHelpCreateTrip(reply_token, msg)
-    }
-    else if (msg === "วิธีการดูแผน") {
-        replyHelpPlan(reply_token, msg)
-    }
-    else if (msg === "วิธีการจัดการบิล") {
-        replyHelpBill(reply_token, msg)
-    }
-    else if (msg === "#แนะนำ") {
-        replyRecommend(reply_token, msg)
-    }
-    else if (msg === "ที่กิน") {
-        replyRecommendEat(reply_token, msg)
-    }
-    else if (msg === "ที่เที่ยว") {
-        replyRecommendTravel(reply_token, msg)
-    }
-    else if (msg === "ที่พัก") {
-        replyRecommendSleep(reply_token, msg)
-    }
-    else if (msg === "#อากาศ") {
-        replyWeather(reply_token, msg)
-    }
-    // get locations here
-    else if (msg === "#ติดต่อ") {
-        replyContact(reply_token, msg)
-    }
-    else if (msg === "#บิล") {
-        replyBill(reply_token, msg)
-    }
-    else if (msg === "#สร้างบิล") {
-        replyCreateBill(reply_token, msg)
-    }
-    else if (msg === "#ดูบิล") {
-        replySeeBill(reply_token, msg)
+    let msg = req.body.events[0]
+    // let msg = req.body.events[0].message.text
+    if (msg.type === "message" && msg.message.type === "text") {
+        if (msg === "อาจารย์โอ๋") {
+            replyProfessor(reply_token, msg)
+        }
+        else if (msg === "#สร้างทริป") {
+            replyCreate(reply_token, msg)
+        }
+        else if (msg === "#ดูแผน") {
+            replyPlan(reply_token, msg)
+        }
+        else if (msg === "ดูแผนทั้งหมด") {
+            replyPlanPerDay(reply_token, msg)
+        }
+        else if (msg === "ดูแผนวันนี้") {
+            replyPlanPerDay(reply_token, msg)
+        }
+        else if (msg === "#ช่วย") {
+            replyHelp(reply_token, msg)
+        }
+        else if (msg === "วิธีการสร้างทริป") {
+            replyHelpCreateTrip(reply_token, msg)
+        }
+        else if (msg === "วิธีการดูแผน") {
+            replyHelpPlan(reply_token, msg)
+        }
+        else if (msg === "วิธีการจัดการบิล") {
+            replyHelpBill(reply_token, msg)
+        }
+        else if (msg === "#แนะนำ") {
+            replyRecommend(reply_token, msg)
+        }
+        else if (msg === "ที่กิน") {
+            replyRecommendEat(reply_token, msg)
+        }
+        else if (msg === "ที่เที่ยว") {
+            replyRecommendTravel(reply_token, msg)
+        }
+        else if (msg === "ที่พัก") {
+            replyRecommendSleep(reply_token, msg)
+        }
+        else if (msg === "#อากาศ") {
+            replyWeather(reply_token, msg)
+        }
+        // get locations here
+        else if (msg === "#ติดต่อ") {
+            replyContact(reply_token, msg)
+        }
+        else if (msg === "#บิล") {
+            replyBill(reply_token, msg)
+        }
+        else if (msg === "#สร้างบิล") {
+            replyCreateBill(reply_token, msg)
+        }
+        else if (msg === "#ดูบิล") {
+            replySeeBill(reply_token, msg)
+        }
+        else if (ev) {
+            replyWeatherMaps(reply_token, msg)
+        }
+    } else if (msg.type === "message" && msg.message.type === "location") {
+        const reply = req => {
+            return request.post({
+              uri: `${LINE_MESSAGING_API}/reply`,
+              headers: LINE_HEADER,
+              body: JSON.stringify({
+                replyToken: req.body.events[0].replyToken,
+                messages: [
+                  {
+                    type: "text",
+                    text: JSON.stringify(req.body)
+                  }
+                ]
+              })
+            });
+          };
+        replyWeatherMaps(reply_token, msg)
     }
     else {
-        reply(reply_token, msg)
+        reply(req);
     }
     res.sendStatus(200)
 })
 
+function replyBased(reply_token, msg) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {EUEqmnC5MpIHn7O3gS9uJ2AJBVt7JCotZj/+t2hOOlBTt7b/+4nPAg/9BFeRawRghXeIeqZe5EMVIexmmEh5c80nwP+BMli10YB6vNFLl38OHFljNNNy1jS9Ft52GmAIUro72i8ebhHfzD9mN9CX1QdB04t89/1O/w1cDnyilFU=}'
+    }
+
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [
+            {
+                type: 'text',
+                text: 'ส่งอะไรมาน้าาาา ผมไม่รู้คับ ฮือ ;_; '
+            }
+        ]
+    })
+
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
 
 function replyProfessor(reply_token, msg) {
     let headers = {
@@ -469,85 +517,107 @@ function replySeeBill(reply_token, msg) {
 //     });
 // }
 
-// function replyWeather(reply_token, msg) {
-//     let headers = {
-//         'Content-Type': 'application/json',
-//         'Authorization': 'Bearer {EUEqmnC5MpIHn7O3gS9uJ2AJBVt7JCotZj/+t2hOOlBTt7b/+4nPAg/9BFeRawRghXeIeqZe5EMVIexmmEh5c80nwP+BMli10YB6vNFLl38OHFljNNNy1jS9Ft52GmAIUro72i8ebhHfzD9mN9CX1QdB04t89/1O/w1cDnyilFU=}'
-//     }
-
-//     let body = JSON.stringify({
-//         replyToken: reply_token,
-//         messages: [
-//             {
-//                 type: "text",
-//                 text: " อยากรู้สภาพอากาศที่ไหนครับ?",
-//                 quickReply: {
-//                     items: [
-//                         {
-//                             type: "action",
-//                             action: {
-//                                 type: "location",
-//                                 label: "Location"
-//                             }
-//                         }
-//                     ]
-//                 }
-//             }
-//         ]
-//     })
-
-//     request.post({
-//         url: 'https://api.line.me/v2/bot/message/reply',
-//         headers: headers,
-//         body: body
-//     }, (err, res, body) => {
-//         console.log('status = ' + res.statusCode);
-//     });
-// }
-
 function replyWeather(reply_token, msg) {
-const functions = require('firebase-functions');
-const request = require('request-promise');
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer {EUEqmnC5MpIHn7O3gS9uJ2AJBVt7JCotZj/+t2hOOlBTt7b/+4nPAg/9BFeRawRghXeIeqZe5EMVIexmmEh5c80nwP+BMli10YB6vNFLl38OHFljNNNy1jS9Ft52GmAIUro72i8ebhHfzD9mN9CX1QdB04t89/1O/w1cDnyilFU=}'
+    }
 
-const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
-const LINE_HEADER = {
-  'Content-Type': 'application/json',
-  'Authorization': 'Bearer {EUEqmnC5MpIHn7O3gS9uJ2AJBVt7JCotZj/+t2hOOlBTt7b/+4nPAg/9BFeRawRghXeIeqZe5EMVIexmmEh5c80nwP+BMli10YB6vNFLl38OHFljNNNy1jS9Ft52GmAIUro72i8ebhHfzD9mN9CX1QdB04t89/1O/w1cDnyilFU=}'
-};
-
-exports.LineBotPush = functions.https.onRequest((req, res) => {
-  return request({
-    method: `GET`,
-    uri: `https://api.openweathermap.org/data/2.5/weather?units=metric&type=accurate&zip=10330,th&appid=f356a3b088a90918fa91d1ace7506841`,
-    json: true
-  }).then((response) => {
-    const message = `City: ${response.name}\nWeather: ${response.weather[0].description}\nTemperature: ${response.main.temp}`;
-    return push(res, message);
-  }).catch((error) => {
-    return res.status(500).send(error);
-  });
-});
-
-const push = (res, msg) => {
-  return request({
-    method: `POST`,
-    uri: `${LINE_MESSAGING_API}/push`,
-    headers: LINE_HEADER,
-    body: JSON.stringify({
-      messages: [
-        {
-          type: `text`,
-          text: msg
-        }
-      ]
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [
+            {
+                type: "text",
+                text: " อยากรู้สภาพอากาศที่ไหนครับ?",
+                quickReply: {
+                    items: [
+                        {
+                            type: "action",
+                            action: {
+                                type: "location",
+                                label: "Location"
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
     })
-  }).then(() => {
-    return res.status(200).send(`Done`);
-  }).catch((error) => {
-    return Promise.reject(error);
-  });
+
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
 }
+
+
+function replyWeatherMaps(reply_token, msg) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImIwYmYzY2E2YzFiZTI3ZjZkOGY5NDFhNjgyMmY2MjE4ZWViMDIzMzYyNDE1NDA3Nzk1NzIxNGYwYTkxNjJiNzljYjZjZTc1MGM4MzEwNGRmIn0.eyJhdWQiOiIyIiwianRpIjoiYjBiZjNjYTZjMWJlMjdmNmQ4Zjk0MWE2ODIyZjYyMThlZWIwMjMzNjI0MTU0MDc3OTU3MjE0ZjBhOTE2MmI3OWNiNmNlNzUwYzgzMTA0ZGYiLCJpYXQiOjE2MDA1MzE1NTEsIm5iZiI6MTYwMDUzMTU1MSwiZXhwIjoxNjMyMDY3NTUxLCJzdWIiOiI5NzkiLCJzY29wZXMiOltdfQ.FMch8VXWx957OH1FjR28JKaHcfxsHV4fSL5Scc_hflnRClf95iND0YaqbxNkiUX7f0TuhxzIMl0GFbrvQ6NKXOvYSj4dfU8jJMuDDG3QZTkLgExXDvQtsH2Ui5ZpAhHgPtkK7lPV1fGsQG_d5Ad9GjOD_oxgZygm6_iSM2ERh76kd2YywxFuN3_pBrBmLOm7kNFYatm_Ntg0Xir7NnnKVsE_S2RYYqsIoM5ZRjrWKK4A0Erk6WiXtXeD0fAFuv-Ope8cG_4Bh3VBgvhbFW3jNZ1OniutHYcba2Bv_P_WoL-xrRzxxvMSIKNXLfneoLNW5HPo0DNV273ZemPgbC6x3LdeFOCxtjy1YA-DZ2tTuQycm32CrD3GJXxQAaYC9TjthkCLXrcu40S6D2jQWOt-brkwhUuY2vNEdVx0YZDZXZ34TWLXYLVoJs-lSMI_NMtMdhhxo29gXr07YJFB7E8NvN2HSVA_Y4lCsz8KIP7ZNhK8QeuR3raxuFPxBM-3e0a-sEiQs3c5NvzEZhLrVD-ldxkCD5t6PdtgUOgaWk9tUfltrZ0sQsga6RFx0sj-R7TvwPHA8oOjarVIc-e-2vixGxipSmXccZ47-SxfAXUn91XfhZwg3zs4UyrVEjy1n62lFLGfmzMSOL05w9fLdJd7cMOs14d67ldnWvXQGV-aQL4'
+    }
+    exports.LineBotPush = functions.https.onRequest((req, res) => {
+        events.location
+        return request({
+            method: `GET`,
+            uri: `https://data.tmd.go.th/nwpapi/v1/forecast/location/daily/at`,
+            json: true
+        }).then((response) => {
+            const message = `City: ${response.name}\nWeather: ${response.weather[0].description}\nTemperature: ${response.main.temp}`;
+            return push(res, message);
+        }).catch((error) => {
+            return res.status(500).send(error);
+        });
+    });
+
+
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [
+            {
+                weather_forecast: {
+                    locations: [
+                        {
+                            location: {
+                                lat: 13.0068,
+                                lon: 100.0829
+                            },
+                            forecasts: [
+                                {
+                                    time: "2017-08-17T00:00:00+07:00",
+                                    data: {
+                                        rh: 88.54,
+                                        tc_max: 28.56
+                                    }
+                                },
+                                {
+                                    time: "2017-08-18T00:00:00+07:00",
+                                    data: {
+                                        rh: 87.44,
+                                        tc_max: 27.21
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        ]
+    })
+
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
 }
+
+
 
 function replyContact(reply_token, msg) {
     let headers = {
@@ -559,40 +629,40 @@ function replyContact(reply_token, msg) {
         replyToken: reply_token,
         messages: [
             {
-                  type: "template",
-                  altText: "This is a buttons template",
-                  template: {
-                      type: "buttons",
-                      thumbnailImageUrl: "https://1.bp.blogspot.com/-U90M8DyKu7Q/W9EtONMCf6I/AAAAAAAAW_4/7L_jB_Rg9oweu2HKhULNdu9WNefw9zf9wCLcBGAs/s1600/sao-full.jpg",
-                      imageAspectRatio: "rectangle",
-                      imageSize: "cover",
-                      imageBackgroundColor: "#FFFFFF",
-                      title: "ติดต่อเจ้าหน้าที่",
-                      defaultAction: {
-                          type: "uri",
-                          label: "contact",
-                          uri: "https://www.google.com"
-                      },
-                      actions: [
-                          {
+                type: "template",
+                altText: "This is a buttons template",
+                template: {
+                    type: "buttons",
+                    thumbnailImageUrl: "https://1.bp.blogspot.com/-U90M8DyKu7Q/W9EtONMCf6I/AAAAAAAAW_4/7L_jB_Rg9oweu2HKhULNdu9WNefw9zf9wCLcBGAs/s1600/sao-full.jpg",
+                    imageAspectRatio: "rectangle",
+                    imageSize: "cover",
+                    imageBackgroundColor: "#FFFFFF",
+                    title: "ติดต่อเจ้าหน้าที่",
+                    defaultAction: {
+                        type: "uri",
+                        label: "contact",
+                        uri: "https://www.google.com"
+                    },
+                    actions: [
+                        {
                             type: "text",
                             label: "อีเมลล์",
                             text: "mrjourney@"
-                          },
-                          {
+                        },
+                        {
                             type: "postback",
                             label: "เพิ่มลงรถเข็น",
                             data: "action=add&itemid=123"
-                          },
-                          {
+                        },
+                        {
                             type: "uri",
                             label: "อ่านรายละเอียด",
                             uri: "https://www.google.com"
-                          }
-                      ]
-                  }
-                
-              }
+                        }
+                    ]
+                }
+
+            }
         ]
     })
 
