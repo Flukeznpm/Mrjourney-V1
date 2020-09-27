06a101d704969592d3d9b86c74ede9bbbecf2ee7
romncn <rom.ncn@gmail.com>
2020-09-27 20:35:06 +0700

    [+] historyRoom profile & AllmyJoinRoom

---
+++ mrjourney-Fe/src/components/Profile/HistoryCreateTrip/HistoryCard.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react';
+import styled from "styled-components";
+import axios from 'axios';
+import {
+    Button as AntButton,
+    Tooltip,
+    Col, Row,
+    Input as AntInput,
+    Select as AntSelect,
+    Progress, Typography
+} from 'antd';
+import momentjs from 'moment'
+
+const ImgCover = styled.img`
+    height: 100px;
+    width: 100%;
+    object-fit: cover;
+    border-top-right-radius: 20px;
+    border-top-left-radius: 20px;
+`;
+
+const { Paragraph } = Typography;
+const AntParagraph = styled(Paragraph)`
+    font-size: 10px;
+    .ant-typography-expand, .ant-typography-edit, .ant-typography-copy {
+        color: gray;
+    }
+    .ant-typography-copy-success, .ant-typography-copy-success:hover, .ant-typography-copy-success:focus {
+        color: ${props => (props.theme.color.primary)};
+    }
+`;
+
+const ColRoomId = styled(Col)`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`;
+
+const ColRoomStatus = styled(Col)`
+    font-size: 12px;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+`;
+
+
+function HistoryCard(props) {
+
+    const [hisRoom, setHistoryRoom] = useState([{}])
+    useEffect(() => {
+        let search = window.location.search;
+        let params = new URLSearchParams(search);
+        let getUserID = params.get('userID');
+        axios.get(`http://localhost:5000/accountProfile/ownerRoom?lineID=${getUserID}`)
+            .then(res => {
+                setHistoryRoom(res.data)
+            })
+    }, [])
+
+    return (
+        <Row gutter={18,18}>
+            {hisRoom.map((history) => {
+                return (
+                    <>
+                        {
+                            history.endDateStatus === false ?
+                                <Col lg={12} md={12} sm={24} xs={24} className="container py-2 d-flex justify-content-center">
+                                    <div class="card" style={{ width: "95%"}}>
+                                        <ImgCover class="card-img-top" src={history.roomCover} alt="Card image cap" />
+                                        <div class="card-body">
+                                            <h5 class="card-title" style={{ fontWeight: "bold" }}>
+                                                {history.roomName}
+                                            </h5>
+                                            <div class="card-text">
+                                                จ. {history.province}
+                                            </div>
+                                            <div class="card-text py-2">
+                                                <button
+                                                    type="button" class="date-room-btn btn p-1 " style={{ fontSize: "12px" }}>
+                                                    {momentjs(history.startDate).format('ll')}                                                        <i class="far fa-calendar-alt ml-2 mr-1"></i>
+                                                </button>
+                                                                &nbsp;-&nbsp;
+                                                                <button
+                                                    type="button" class="date-room-btn btn p-1 " style={{ fontSize: "12px" }}>
+                                                    {momentjs(history.endDate).format('ll')}                                                        <i class="far fa-calendar-alt ml-2 mr-1"></i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Col>
+                                :
+                                null
+                        }
+                    </>
+                )
+            })}
+        </Row>
+    )
+}
+export default HistoryCard;
