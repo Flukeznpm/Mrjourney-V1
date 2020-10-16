0e44a4c323113d85b22a249a238ebdb6b689624c
romncn <rom.ncn@gmail.com>
2020-10-17 01:36:30 +0700

    +

--- mrjourney-Fe/src/components/Home/ShowRoomBox.js
+++ mrjourney-Fe/src/components/Home/ShowRoomBox.js
@@ -43,7 +43,7 @@ function ShowRoomBox(props) {
     const [filterRoomProvince, setFilterRoomProvince] = useState(null)
     const { Option } = AntSelect;
     useEffect(() => {
-        axios.get('http://localhost:5000/room')
+        axios.get('https://mrjourney-senior.herokuapp.com/room')
             .then(async res => {
                 const sortArray = type => {
                     const types = {
