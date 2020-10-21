c304ec49290b2fae30b41a86b8cfb5491b0d99c7
romncn <rom.ncn@gmail.com>
2020-10-21 23:00:21 +0700

    [+] check to show flex message

---
+++ mrjourney-Be/controller/botController.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var firebase = require('firebase-admin');
+const { firestore } = require('firebase-admin');
+let db = firebase.firestore();
+
+async function checkTripAvaliable(data) {
+    let status = false;
+    const checkTripIDRef = db.collection('LineGroup').doc(data).collection('Trip').where('tripStatus', '==', true);
+    await checkTripIDRef.get().then(async snapshot => {
+        if (snapshot.empty) {
+            status = false;
+        } else {
+            status = true;
+        }
+    })
+    return status;
+};
+
+module.exports = { checkTripAvaliable };
