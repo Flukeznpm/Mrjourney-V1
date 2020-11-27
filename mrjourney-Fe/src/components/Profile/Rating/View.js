b89d5927c1e6d7ae71ad7cb387ecacd3d02480f6
romncn <rom.ncn@gmail.com>
2020-11-27 14:29:21 +0700

    [+] UI Rating view

--- mrjourney-Fe/src/components/Profile/Rating/View.js
+++ mrjourney-Fe/src/components/Profile/Rating/View.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
 import axios from 'axios';
-import jwt from 'jsonwebtoken';
-import cookie from 'react-cookies'
-import { EditOutlined } from '@ant-design/icons';
 import {
     Card,
     Row, Col,
     Button as AntButton,
+    Progress
 } from 'antd';
 
 const AntCard = styled(Card)`
@@ -15,6 +13,12 @@ const AntCard = styled(Card)`
   box-shadow: 2px 8px 10px rgba(0, 0, 0, 0.06), 0px 3px 4px rgba(0, 0, 0, 0.07);
   margin: 10px 0px 10px 0px;
   padding: 15px 0px 15px 0px;
+`;
+
+const ProgressScore = styled(Progress)`
+    .ant-progress-inner:not(.ant-progress-circle-gradient) .ant-progress-circle-path {
+        stroke: ${props => (props.theme.color.primary)};
+    }
 `;
 
 function View(props) {
@@ -41,21 +45,24 @@ function View(props) {
                     <Row justify="space-between">
                         <Col span={6} className="text-center">
                             ความเพรียบพร้อม
-                            <AntCard style={{ padding: 0 }}>
+                            {/* <AntCard style={{ padding: 0 }}>
                                 {(rating.preparationScore/rating.countOfSubmit).toFixed(2)}/5
-                            </AntCard>
+                            </AntCard> */}
+                            <ProgressScore type="circle" percent={((rating.preparationScore/rating.countOfSubmit)/5)*100} format={() => `${(rating.preparationScore / rating.countOfSubmit).toFixed(2)}/5`} />
                         </Col>
                         <Col span={6} className="text-center">
                             ความคุ้มค่า
-                            <AntCard style={{ padding: 0 }}>
-                                {(rating.entertainmentScore/rating.countOfSubmit).toFixed(2)}/5
-                            </AntCard>
+                            {/* <AntCard style={{ padding: 0 }}> */}
+                            {/* {(rating.entertainmentScore/rating.countOfSubmit).toFixed(2)}/5 */}
+                            <Progress type="circle" percent={((rating.entertainmentScore/rating.countOfSubmit)/5)*100} format={() => `${(rating.entertainmentScore / rating.countOfSubmit).toFixed(2)}/5`} />
+                            {/* </AntCard> */}
                         </Col>
                         <Col span={6} className="text-center">
                             ความสนุก
-                            <AntCard style={{ padding: 0 }}>
-                                {(rating.worthinessScore/rating.countOfSubmit).toFixed(2)} /5
-                            </AntCard>
+                            {/* <AntCard style={{ padding: 0 }}> */}
+                            {/* {(rating.worthinessScore/rating.countOfSubmit).toFixed(2)} /5 */}
+                            <Progress type="circle" percent={((rating.worthinessScore/rating.countOfSubmit)/5)*100} format={() => `${(rating.worthinessScore / rating.countOfSubmit).toFixed(2)}/5`} />
+                            {/* </AntCard> */}
                         </Col>
                     </Row>
                 )
